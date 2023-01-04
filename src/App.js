a8ead4b69066077f8ff7d8f2ea4dd46bef4a0a6b
Lssport <111240117+lssport@users.noreply.github.com>
2023-01-05 00:07:07 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { ProductList } from './components/ProductList';
+import { Home } from './pages/Home';
+
+function App() {
+    const [products] =
+useState ([
+   {
+    id: 1,
+    name: 'Product 1', 
+    price: 9.99,
+    description: 'This is Product 1'
+   }, 
+   {
+     id: 2,
+     name: 'Product 2', 
+     price: 14.99,
+     description: 'This is Product 2'
+   }, 
+   {
+     id: 3,
+     name: 'Product 3', 
+     price: 19.99,
+     description: 'This is Product 3'
+   }  
+ ]);
+   return (
+     <Home>
+       <ProductList products={products} />
+      </Home>
+   );
+}
+export default App;
