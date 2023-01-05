f377c8bcb8eac585c18e117e4434612688b275f3
Lssport <111240117+lssport@users.noreply.github.com>
2023-01-05 23:42:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
+import React from "react";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 
 const rootElement = document.getElementById('root');
